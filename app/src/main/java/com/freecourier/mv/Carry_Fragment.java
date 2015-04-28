e443ab550d71cab7bf16dff595ca838df299bab4
vrajakishore <vrajakishore2@gmail.com>
2015-04-28 14:36:37 +0530

    Facebook Login included and working

---
+++ app/src/main/java/com/freecourier/mv/Carry_Fragment.java
@@ -0,0 +1,19 @@
+package com.freecourier.mv;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Carry_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.carrier_layout, container, false);
+        return rootview;
+    }
+}
---
+++ app/src/main/java/com/freecourier/mv/Receive_Fragment.java
@@ -0,0 +1,18 @@
+package com.freecourier.mv;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Receive_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.receive_layout, container, false);
+        return rootview;
+    }
+}

fd9d243e98e3fc2d18300c149ce23f70f333bae7
vrajakishore <vrajakishore2@gmail.com>
2015-04-30 17:15:46 +0530

    SignIn Button corrected
    And Login logic added in MainActivity

--- app/src/main/java/com/freecourier/mv/MainActivity.java
+++ app/src/main/java/com/freecourier/mv/MainActivity.java
@@ -1,17 +1,44 @@
 package com.freecourier.mv;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private EditText username;
+    private EditText password;
+
+    private static final String TAG = "your activity name";
+
 
     public static final int INDEX_SIMPLE_LOGIN = 0;
     public static final int INDEX_CUSTOM_LOGIN = 1;
@@ -25,7 +52,6 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFragmentManager = getSupportFragmentManager();
-
     }
 
     @Override
@@ -40,12 +66,13 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    public void send_to_menu(View view)
-    {
-
-        Intent intent = new Intent(this, Main_Navigation.class);
-        // Intent intent = new Intent(MainActivity.this, Signup_Page.class);
-        startActivity(intent);
+    public void send_to_menu(View view) {
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        String []args = new String[2];
+        args[0] = username.getText().toString().trim();
+        args[1] = password.getText().toString().trim();
+        new RetrieveFeedTask().execute(args);
     }
 
     @Override
@@ -86,4 +113,53 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
+    class RetrieveFeedTask extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+
+        @Override
+        protected String doInBackground(String[] args) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            String url = "http://172.16.32.54:8888/rest/user/login/";
+            HttpPost request = new HttpPost(url);
+            String responseStr = "";
+            try {
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("email", args[0]));
+                nameValuePairs.add(new BasicNameValuePair("password", args[1]));
+                request.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = client.execute(request);
+                Log.d(TAG, "input = " + args[0]+" - "+args[1]);
+                responseStr = EntityUtils.toString(response.getEntity());
+                Log.d(TAG, "outcome = " + responseStr);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "["+responseStr+"]";
+        }
+
+        protected void onPostExecute(String result) {
+            // TODO: check this.exception
+            // TODO: do something with the feed
+            try {
+                JSONArray json = new JSONArray( result);
+                //Log.d("error out", "in onPostExecute results123 : " + result);
+                JSONObject jsonobj = json.getJSONObject(0);
+                String message = jsonobj.getString("message");
+                Log.d("error out", "in onPostExecute message : " + message);
+                if(message.equalsIgnoreCase("success")){
+                    Intent intent = new Intent(MainActivity.this, Main_Navigation.class);
+                    // Intent intent = new Intent(MainActivity.this, Signup_Page.class);
+                    startActivity(intent);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
+
